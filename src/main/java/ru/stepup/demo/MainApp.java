4ccf0aa77765d66b707da8de34f7cf88bf505431
Valerii Tilkov <vatilkov@gmail.com>
2025-08-05 16:31:18 +0300

    initial commit

---
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -0,0 +1,19 @@
+package ru.stepup.demo;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+        String a = String.valueOf("1");
+        String b = new String("1");
+        if (a.equals(b)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        ValueC vc = new ValueC("1");
+        vc.setC(5);
+        System.out.println(vc.getC());
+    }
+}
---
+++ src/main/java/ru/stepup/demo/ValueC.java
@@ -0,0 +1,17 @@
+package ru.stepup.demo;
+
+public class ValueC {
+    private String c;
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(int a){
+        this.c = String.valueOf(a);
+    }
+
+    public ValueC(String c) {
+        this.c = c;
+    }
+}
